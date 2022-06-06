caede8edeff4c0624fee52813a14fec9e1b42015
guruck <guruck@gmail.com>
2022-06-06 20:12:42 -0300

    desafio 3 - Livro

---
+++ desafio3/src/desafio3/pack/gft/Livro.java
@@ -0,0 +1,15 @@
+package desafio3.pack.gft;
+
+public class Livro {
+	private String nome;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	
+}
