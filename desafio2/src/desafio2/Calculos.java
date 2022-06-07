4b660b577a24f8e3172af12543b6635399f4bf65
guruck <guruck@gmail.com>
2022-06-07 17:42:50 -0300

    2

--- desafio2/src/desafio2/Calculos.java
+++ desafio2/src/desafio2/Calculos.java
@@ -6,13 +6,28 @@ import java.io.InputStreamReader;
 public class Calculos {
 
 	public static void main(String[] args) {
-		for (int i = 1; i <= 10; i++) {
+		Integer[] lista = new Integer[10]; 
+		
+		for (int i = 0; i < 10; i++) {
 			System.out.println("Digite um numero ["+i+"]:");
-			int n = readInt();
-			
+			lista[i] = readInt();
 		}
-		
-		
+		sort(lista);
+		for (Integer num:lista) {
+			System.out.print(num+ " ");
+		}
+	}
+	
+	public static void sort(Integer[] lista) {
+		for (int i=0; i<lista.length-1;i++) {
+			for(int j=1;j<lista.length;j++) {
+				if (lista[i]<lista[j]) {
+					Integer aux = lista[i];
+					lista[i]=lista[j];
+					lista[j]=aux;
+				}
+			}
+		}
 	}
 	
 	public static String readString() {
